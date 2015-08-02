34a1d1b53edd3229c31bbdf3319eac1e4eb304bb
Aleksandr Sandrovskiy <asandrovsky@gmail.com>
2015-08-02 11:21:41 +0700

    Cleanup

--- conductor.go
+++ conductor.go
@@ -5,40 +5,8 @@ import (
 	"reflect"
 )
 
-type Conductor struct {
-	sync.WaitGroup
-	limiter chan bool
-	limited bool
-}
-
-func (c *Conductor) Go(fn func(args ...interface{}), args ...interface{}) {
-	c.Add(1)
-	if c.limited {
-		c.limiter <- true
-	}
-
-	wrapped := func() {
-		fn(args...)
-
-		c.Done()
-		if c.limited {
-			<-c.limiter
-		}
-	}
-	go wrapped()
-}
-
-func (c *Conductor) Limit(limit int) {
-	if limit > 0 {
-		c.limited = true
-		c.limiter = make(chan bool, limit)
-	}
-}
-
-var limiter chan bool
-
 func Limit(f interface{}, l int) func(args ...interface{}) {
-	limiter = make(chan bool, l)
+	limiter := make(chan bool, l)
 	fValue := reflect.ValueOf(f)
 
 	wrapped := func(args ...interface{}) {
