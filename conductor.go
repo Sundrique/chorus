007b5734e046aa4b0af34a2d366c97f66925c9fb
Aleksandr Sandrovskiy <asandrovsky@gmail.com>
2015-08-02 11:02:12 +0700

    Add Limit and Wait decorators

--- conductor.go
+++ conductor.go
@@ -2,6 +2,7 @@ package chorus
 
 import (
 	"sync"
+	"reflect"
 )
 
 type Conductor struct {
@@ -32,4 +33,43 @@ func (c *Conductor) Limit(limit int) {
 		c.limited = true
 		c.limiter = make(chan bool, limit)
 	}
+}
+
+var limiter chan bool
+
+func Limit(f interface{}, l int) func(args ...interface{}) {
+	limiter = make(chan bool, l)
+	fValue := reflect.ValueOf(f)
+
+	wrapped := func(args ...interface{}) {
+		limiter <- true
+
+		argsValues := []reflect.Value{}
+		for _, v := range args {
+			argsValues = append(argsValues, reflect.ValueOf(v))
+		}
+		fValue.Call(argsValues)
+
+		<-limiter
+	}
+	return wrapped
+}
+
+func Wait(f interface{}, wg sync.WaitGroup) func(args ...interface{}) {
+
+	fValue := reflect.ValueOf(f)
+
+	wrapped := func(args ...interface{}) {
+		wg.Add(1)
+
+		argsValues := []reflect.Value{}
+		for _, v := range args {
+			argsValues = append(argsValues, reflect.ValueOf(v))
+		}
+		fValue.Call(argsValues)
+
+		wg.Done()
+	}
+
+	return wrapped
 }
--- conductor_test.go
+++ conductor_test.go
@@ -3,6 +3,7 @@ package chorus
 import (
 	. "gopkg.in/check.v1"
 	"time"
+	"sync"
 )
 
 type ConductorSuite struct{}
@@ -69,6 +70,53 @@ func (s *ConductorSuite) TestGoWithArguments(c *C) {
 	}
 
 	conductor.Wait()
+
+	c.Check(sum, Equals, 45)
+}
+
+func (s *ConductorSuite) TestLimit(c *C) {
+	limit := 3
+
+	concurrentCount := 0
+	var wg sync.WaitGroup
+
+	f := func() {
+		concurrentCount++
+		if concurrentCount > limit {
+			c.Fail()
+		}
+
+		time.Sleep(100)
+
+		concurrentCount--
+		wg.Done()
+
+	}
+
+	wg.Add(10)
+
+	limitedF := Limit(f, limit)
+	for i := 0; i < 10; i++ {
+		go limitedF()
+	}
+	wg.Wait()
+}
+
+func (s *ConductorSuite) TestWait(c *C) {
+	var sum int
+
+	f := func(args ...interface{}) {
+		sum += args[0].(int)
+	}
+
+	var wg sync.WaitGroup
+	fWait := Wait(f, wg)
+
+	for i := 0; i < 10; i++ {
+		fWait(i)
+	}
+
+	wg.Wait()
 
 	c.Check(sum, Equals, 45)
 }
